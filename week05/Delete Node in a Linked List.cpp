f1092b94cf3824baee43d9bc2a57c4c036edde8f
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2022-06-10 16:39:12 +0600

    Delete Node in a Linked List

---
+++ week05/Delete Node in a Linked List.cpp
@@ -0,0 +1,11 @@
+// TC: O(1);
+// SC: O(1);
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* dumpNode = node->next;
+        node->val = node->next->val;
+        node->next = node->next->next;
+        delete dumpNode;
+    }
+};
